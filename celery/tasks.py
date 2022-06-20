e59120c4392a465a7ea33e66e5dbbca76835b2d9
Elianagam <elianagam2@gmail.com>
2022-06-20 11:44:29 -0300

    Add celery task

--- celery/tasks.py
+++ celery/tasks.py
@@ -52,3 +52,41 @@ def user_register(self, user_id):
                 'exc_message': traceback.format_exc().split('\n')
             })
         raise ex
+
+
+@celery.task(name='user.login.federated', bind=True, acks_late=True)
+def user_register(self, user_id):
+    register = 0
+    try:
+        LOGGER.info('Starting register task')
+        register += 1
+        LOGGER.info('Finished register task')
+        return {"result": f"New user register {user_id}",
+                "total": f"{register}"}
+    except Exception as ex:
+        self.update_state(
+            state=states.FAILURE,
+            meta={
+                'exc_type': type(ex).__name__,
+                'exc_message': traceback.format_exc().split('\n')
+            })
+        raise ex
+
+
+@celery.task(name='user.register.federated', bind=True, acks_late=True)
+def user_register(self, user_id):
+    register = 0
+    try:
+        LOGGER.info('Starting register task')
+        register += 1
+        LOGGER.info('Finished register task')
+        return {"result": f"New user register {user_id}",
+                "total": f"{register}"}
+    except Exception as ex:
+        self.update_state(
+            state=states.FAILURE,
+            meta={
+                'exc_type': type(ex).__name__,
+                'exc_message': traceback.format_exc().split('\n')
+            })
+        raise ex
